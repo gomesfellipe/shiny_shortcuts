6fb10cbb4378a73cf4423fa04adec2585d43231e
Fellipe <gomes.fellipe@hotmail.com>
2019-06-28 19:29:12 -0300

    Update download_zip.R

--- download_zip.R
+++ download_zip.R
@@ -1,25 +1,27 @@
 # ui:
-#
+
 # Botao de download:
-# downloadButton("Zip", "Download das bases de apoio")
+downloadButton("Zip", label = "Download das bases de apoio")
 
 
 # server:
-# 
-# output$Zip <-
-#   downloadHandler(
-#     # Cria objeto para download
-#     filename = "apoio.zip",
-#     # Nome do arquivo a ser salvo
-#     
-#     content = function(file) {
-#       
-#       zip::zip(zipfile = file,
-#                files = dir("dep/temp", full.names = TRUE))
-#       
-#       if (file.exists(paste0(file, ".zip")))
-#         file.rename(paste0(file, ".zip"), file)
-#       
-#     },
-#     contentType = "application/zip"
-#   )
+
+# Criar Zip com arquivo xlsx
+# https://github.com/gomesfellipe/shiny_shortcuts/blob/master/download_zip.R
+output$Zip <-
+  downloadHandler(
+    # Cria objeto para download
+    filename = "apoio.zip",
+    # Nome do arquivo a ser salvo
+
+    content = function(file) {
+
+      zip::zip(zipfile = file,
+               files = dir("data/output_data", full.names = TRUE))
+
+      if (file.exists(paste0(file, ".zip")))
+        file.rename(paste0(file, ".zip"), file)
+
+    },
+    contentType = "application/zip"
+  )
