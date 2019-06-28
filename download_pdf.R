038e5cd39b0900df4f0f14511a6f5eaa0d04032c
Fellipe <gomes.fellipe@hotmail.com>
2019-06-28 19:27:52 -0300

    Update download_pdf.R

--- download_pdf.R
+++ download_pdf.R
@@ -3,24 +3,30 @@
 # baseado no input do usuario
 
 # ui:
-#
+
 # Botao de download:
-# downloadButton("Relatorio", "Download")
+downloadButton("Relatorio", "Download")
 # Input da data:
-# dateInput("date", value = "`Sys.time()`")
+textInput("text", h3("Insira o número do relatorio:"),value = " ")
 
 # server:
-# output$Relatorio <-
-#   downloadHandler(
-#     filename = "report.pdf",
-#     
-#     content = function(file) {
-#       
-#       report <- "script_06_gerar_relatorio.Rmd"
-#       # Parametros enviados ao arquivo .Rmd
-#       params          <- list(now = input$date)
-#       # Renderiza .Rmd
-#       rmarkdown::render(report, output_file = file,
-#                         encoding = "UTF-8", params = params)
-#     }
-#   )
+
+ # Renderiza o documento rmarkdown/latex
+  observeEvent(input$go, {
+    # https://github.com/gomesfellipe/shiny_shortcuts/blob/master/download_pdf.R
+    output$Relatorio <-
+      downloadHandler(
+        filename = "report.pdf",
+        
+        content = function(file) {
+          
+          report <- "src/relatorio.Rmd"
+          # Parametros enviados ao arquivo .Rmd
+          params          <- list(date = input$date,
+                                  text = input$text)
+          # Renderiza .Rmd
+          rmarkdown::render(report, output_file = file,
+                            encoding = "UTF-8", params = params)
+        }
+      )
+  })
